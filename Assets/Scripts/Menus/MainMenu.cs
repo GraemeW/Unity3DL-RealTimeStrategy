90e0050a586b140384491eec1fa53bff128d4daa
GraemeMW <graememwilliams@gmail.com>
2021-08-15 21:57:01 -0700

    Add team transport support

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using Steamworks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,38 @@ public class MainMenu : MonoBehaviour
     // Tunables
     [SerializeField] GameObject landingPagePanel = null;
     [SerializeField] GameObject enterAddressPanel = null;
+    [SerializeField] bool useSteam = false;
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequired;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+
+    private void Start()
+    {
+        if (!useSteam) { return; }
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequired = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
 
     public void HostLobby()
     {
         landingPagePanel.SetActive(false);
+
+        if (useSteam)
+        {
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            return;
+        }
 
         NetworkManager.singleton.StartHost();
     }
 
     public void JoinLobby()
     {
+        if (useSteam) { return; }
+
         landingPagePanel.SetActive(false);
         enterAddressPanel.SetActive(true);
     }
@@ -25,5 +48,40 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if(callback.m_eResult != EResult.k_EResultOK)
+        {
+            landingPagePanel.SetActive(true);
+            return;
+        }
+
+        NetworkManager.singleton.StartHost();
+
+        SteamMatchmaking.SetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            "HostAddress",
+            SteamUser.GetSteamID().ToString());
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active) { return; }
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            "HostAddress");
+
+        NetworkManager.singleton.networkAddress = hostAddress;
+        NetworkManager.singleton.StartClient();
+
+        landingPagePanel.SetActive(false);
     }
 }
